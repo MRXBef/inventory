bfac2758ce1392c169331e6d60dc7ace1126e06b
MRXbef <adambayu878@gmail.com>
2024-08-03 20:07:09 +0800

    Mengubah tampilan outlet box

--- client/src/components/Outlet.jsx
+++ client/src/components/Outlet.jsx
@@ -9,6 +9,7 @@ import { useEffect } from 'react'
 
 const Outlet = () => {
     const [outlets, setOutlets] = useState([])
+    const [msg, setMsg] = useState(null)
 
     useEffect(() => {
         getOutlet()
@@ -23,11 +24,26 @@ const Outlet = () => {
             }
         } catch (error) {
             console.log(error)
+            setMsg({msg: error.response.data.msg, color: 'red'})
         }
     }
   return (
     <div className='is-flex'>
         <Sidebar/>
+
+        {msg ? 
+            <>
+                <div className='messages' style={{ backgroundColor: msg.color }}>
+                    <p>{msg.msg}</p>
+                    <p style={{display: 'none'}}>
+                        {setTimeout(() => {setMsg(null)}, 3000)}
+                    </p>
+                </div>
+            </>
+            :
+            ''
+        }
+
         <div className='outletContainer'>
             <div className="outletHeader">
                 <div className="judul">
@@ -46,18 +62,18 @@ const Outlet = () => {
                                 </i>
                                 <h1 style={{fontSize: '25px', color: 'darkorange'}}>{outlet.name}</h1>
                             </div>
-                            <div style={{display: 'flex', gap: '5px', marginBottom: '35px'}}>
+                            <div style={{display: 'flex', gap: '5px', marginBottom: '55px'}}>
                                 <i >
                                     <CIcon icon={icon.cilLocationPin}/>
                                 </i>
                                 <p>{outlet.address}</p>
                             </div>
-                            <div style={{display: 'flex', gap: '5px', position: 'absolute', bottom: '10px'}}>
+                            <div style={{display: 'flex', gap: '5px', position: 'absolute', bottom: '25px', borderTop: '1px solid grey', width: '85%', paddingTop: '10px'}}>
                                 <i >
                                     <CIcon style={{color: 'green'}} icon={icon.cilPhone}/>
                                 </i>
                                 <p>{outlet.phone}</p>
-                                <a href={`https://wa.me/+62${outlet.phone}`} target="_blank" className='button' style={{height: '30px', marginTop: '-5px', backgroundColor: 'green', outlet: 'none', border: 'none', position: 'absolute', left: '220px', bottom: '5px', padding: '20px'}}>
+                                <a href={`https://wa.me/+62${outlet.phone}`} target="_blank" className='button' style={{height: '30px', marginTop: '-5px', backgroundColor: 'green', outlet: 'none', border: 'none', position: 'absolute', left: '220px', bottom: '-13px', padding: '20px'}}>
                                     <i style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}><CIcon icon={icon.cilChatBubble}/></i>
                                 </a>
                             </div>
